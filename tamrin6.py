2d30e429e999e2dfe95fedf2b0b5740d3708a08c
mehranmoqaddam <122170859+mehranmoqaddam@users.noreply.github.com>
2023-01-22 22:04:10 +0300

    Add files via upload

---
+++ tamrin6.py
@@ -0,0 +1,13 @@
+import random
+n = int(input("tedade adad random : "))
+list1 = []
+#list1 = adade ba tekrar
+for i in range(n):
+    i += 1
+    r = (random.randint(0,100))
+    list1.append(r)
+list2 = set(list1)
+#list2 = adade beedune tekrar
+print("liste ba tekrar " , list1)
+print("liste bi tekrar " , list2)
+
---
+++ tamrin7.py
@@ -0,0 +1,11 @@
+n = int(input("tedad e donbale fibo : "))
+
+fibonacci = [1,2]
+
+for i in range(2,n):
+
+    formul = fibonacci[i-1]+fibonacci[i-2]
+
+    fib.append(formul)
+
+print(fibonacci)
